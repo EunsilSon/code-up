2932c2a8f946c6f8d7745b9a74fccb0ec30bf6f5
EunsilSon <eunsil1023@gmail.com>
2023-02-01 01:00:12 +0900

    데이터형 1028 ~ 1030

---
+++ src/j1030.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class j1030 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextLong());
+    }
+}
